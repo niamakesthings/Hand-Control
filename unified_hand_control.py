9d83ee656223e879ded3e81adedb51f19b5754a8
Nia <itsniakitty@gmail.com>
2026-07-19 00:26:56 -0500

    Config loading, multifinger tracking

--- unified_hand_control.py
+++ unified_hand_control.py
@@ -1,14 +1,14 @@
 # IMPORTANT: For now, this project only supports Linux desktops
-# I might see if I can expand it to windows in the future; macOS is unlikely since:
+# I might see if I can expand it to Windows in the future; macOS is unlikely since:
 #   a) I don't have a mac, and
 #   b) I don't have the money to buy a mac.
 # Everything here is tested in a CachyOS + Niri configuration, and generally should work without modifications
 # on any linux system running a Niri DE.
-# For other DEs, please edit the commands in the ACTIONS dictionary to whatever suits your use case.
+# For other DEs, please edit the commands in config.json to whatever suits your use case.
 
 # Also, the gestures may be finicky depending on the quality of your webcam, lighting, etc. My recommendation
 # is to hold your palm out with all fingers extended, then practice a bit with just your index finger.
-# You may also need to tweak the presets in config.json until they are to your liking.
+# You may also need to tweak the movement presets in config.json until they are to your liking.
 
 
 import subprocess
@@ -24,71 +24,157 @@ import json
 
 
 # Landmark constants for ease of use: NOTE: DO NOT CHANGE!
-WRIST_LANDMARK = 0
-THUMB_LANDMARK = 4
-INDEX_LANDMARK = 8
-MIDDLE_LANDMARK = 12
-RING_LANDMARK = 16
-PINKY_LANDMARK = 20
-
-
-#NOTE: All following constants are now handled by the load_preferences() function and config.json file.
-# Model path: Change this if using a custom model
-MODEL_PATH = None
-
-# Evaluation constants
-WINDOW_SECONDS = None # How recent a movement must be to be considered for a swipe
-COOLDOWN_SECONDS = None # Minimum triggering time between swipes
-
-# Movement thresholds
-# NOTE: This may be deprecated in a future commit for a 4-axis system (up/down/left/right)
-SWIPE_THRESHOLD_X = None
-SWIPE_THRESHOLD_Y = None
-
-
-
-# Dictionary of all gesture actions and corresponding commands. sorted() ensures that the right order is used every time
-# Format for actions is finger_direction
-# HACK: This dictionary is maybe one of the ugliest pieces of code I have ever written and will probably be changed later.
-ACTIONS = {
-    #sorted(["ACTIONS", "GO", "HERE"]): ["COMMAND", "GOES", "HERE"],\
-    sorted(["index_down"], "niri msg action focus-workspace-down".split())
-    sorted(["index_up"], "niri msg action focus-workspace-up".split())
-
+LANDMARKS = {
+    "wrist": 0,
+    "thumb": 4,
+    "index": 8,
+    "middle": 12,
+    "ring": 16,
+    "pinky": 20,
 }
 
-def load_preferences(json_path: str){
+def load_json(json_path: str):
+    # Attempt to open and load data from config.json
     try:
         with open(json_path) as handler:
-            prefs = json.load(handler)
+            return json.load(handler)
 
-        MODEL_PATH = prefs["model_path"]
-
-        WINDOW_SECONDS = prefs["evaluation"]["window_seconds"]
-        COOLDOWN_SECONDS = prefs["evaluation"]["cooldown_seconds"]
-
-        SWIPE_THRESHOLD_X = prefs["movement"]["swipe_threshold_x"]
-        SWIPE_THRESHOLD_Y = prefs["movement"]["swipe_threshold_y"]
+    except FileNotFoundError as e:
+        print("Error: Application would not locate config.json file. Is it located in the same directory as the application file?")
+        raise
     
-    except FileNotFoundError:
-        print("Error: Application could not locate config.json file.")
+    except PermissionError as e:
+        print("Error: Application does not have read permissions for config.json. Consider using chmod?")
+        raise
+
+def init_landmarker(model_path: str):
+    # Set up landmarker
+    landmarker_options = vision.HandLandmarkerOptions(
+        base_options=mp_python.BaseOptions(model_asset_path=model_path),
+        running_mode=vision.RunningMode.VIDEO,
+        num_hands=1
+    )
+
+    landmarker = vision.HandLandmarker.create_from_options(landmarker_options)
+    return landmarker
+
+def init_cam(webcam_index: int):
+    # Set up camera capture
+    cam = cv2.VideoCapture(webcam_index)
+    if not cam.isOpened:
+        raise RuntimeError("Could not open webcam at index " + WEBCAM_INDEX + ". Your webcam is at a different index? The appropriate drivers aren't installed?")
+    return cam
+
+def init_buffer(landmarks):
+    # Set up history buffer and timestamp tracking NOTE: Right now the implementation is only for the index finger
+    history = {}
+    for key in landmarks.keys():
+        history[key] = deque()
     
-    except PermissionError:
-        print("Error: Application does not have read permissions for config.json")
-}
+    return history
 
-def run_action():
+def get_results(frame, frame_index, landmarker):
+    rgb_cvt = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_cvt)
+    timestamp_ms = int(frame_index * 1000 / 30) # 1s = 1000ms, 30f = 1s
+    results = landmarker.detect_for_video(mp_image, timestamp_ms)
+    return results
+
+def main() -> None:
+    settings = load_json('config.json')
 
 
-def process_movement():
+    # Initialize constants based on loaded data in "settings"
+    try:
+        WEBCAM_INDEX = settings["webcam"]["index"]
+        SHOW_FEED = settings["webcam"]["show_feed"]
+        MODEL_PATH = settings["model_path"]
 
-def handle_feed(show: bool, frame):
-    if show:
-        cv2.imshow("Hand Control Preview", frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+        WINDOW_SECONDS = settings["evaluation"]["window_seconds"] # Amount of past time the program analyzes to detect a swipe
+        COOLDOWN_SECONDS = settings["evaluation"]["cooldown_seconds"] # Minimum amount of time the program allows between swipes
+
+        SWIPE_THRESHOLD_X = settings["movement"]["swipe_threshold_x"] # Minimum percentage of the screen a swipe needs to travel horizontally across to be registered.
+        SWIPE_THRESHOLD_Y = settings["movement"]["swipe_threshold_y"] # Same as above, but vertically
+
+        ACTIONS = settings["actions"] # Dictionary of all triggerable gestures and corresponding commands
+    
+    except KeyError as e:
+        print("Error: Could not load settings from config.json. File may be malformed.")
+        raise
+
+
+
+    landmarker = init_landmarker(MODEL_PATH)
+    cam = init_cam(WEBCAM_INDEX)
+
+    history = init_buffer(LANDMARKS)
+    last_trigger_time = 0.0
+    frame_index = 0
+
+    try:
+        while True:
+            active, frame = cam.read()
+            if not active:
+                break
+            
+            results = get_results(frame, frame_index, landmarker)
+            frame_index += 1
+            now = time.time()
+
+            if results.hand_landmarks:
+                positions = {}
+                for key, landmark in LANDMARKS.items():
+                    positions[key] = results.hand_landmarks[0][landmark] # [0] refers to the first hand catagorized. For now, there is only single hand support.
+                    history[key].append((now, positions[key].x, positions[key].y))
+
+                    while history[key] and now - history[key][0][0] > WINDOW_SECONDS:
+                        history[key].popleft()
+                
+                if now - last_trigger_time > COOLDOWN_SECONDSS!!: # len(history) ensures there's at least 2 data points to extrapolate based off of.
+
+                    deltas = {}
+                    for key in positions.keys():
+                        if len(history[key]) > 1:
+                            _, x0, y0 = history[key][0]
+                            _, x1, y1 = history[key][-1]
+                            deltas[key] = (x1-x0, y1-y0)
+                        else:
+                            deltas[key] = (0, 0)
+                    
+
+                    # Total measure of the "influence" of each direction so that the largest may be chosen with respect to thresholds
+                    x_ratio = abs(deltas["index"][0]) / SWIPE_THRESHOLD_X
+                    y_ratio = abs(deltas["index"][1]) / SWIPE_THRESHOLD_Y
+
+                    
+                    if x_ratio >= 1.0 or y_ratio >= 1.0:
+                        if x_ratio > y_ratio:
+                            direction = "left" if deltas["index"][0] < 0 else "right"
+                        else:
+                            direction = "up" if deltas["index"][1] < 0 else "down"
+                    
+                        result = subprocess.run(ACTIONS["swipe"]["one_finger"][direction].split())
+                        last_trigger_time = now
+                        history["index"].clear()
+
+            else:
+                for key in history.keys():
+                    history[key].clear()
+
+            if SHOW_FEED:
+                cv2.imshow("Debug Panel", frame)
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+
+    
+    finally:
+        cam.release()
+        cv2.destroyAllWindows()
+
+
 
 
 if __name__ == "__main__":
+    main()
 
 
